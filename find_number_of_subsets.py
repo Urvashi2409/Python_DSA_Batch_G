2eb51083fdf24155b981f87e6f444d9d28e86fa4
Urvashi2409 <urvashicode2410@gmail.com>
2023-09-01 21:55:08 +0530

    list slicing, binary to decimal, decimal to binary

---
+++ find_number_of_subsets.py
@@ -0,0 +1,9 @@
+
+
+
+
+# string = input("Enter a string: ")
+# l = len(string)
+# num_of_subsets = 2 ** l 
+
+# for i in range(0, num_of_subsets-1):
---
+++ list.py
@@ -0,0 +1,6 @@
+# mylist = [2, 3, 4, 5, 67, 3]
+# print(mylist[::])
+# print(mylist[-2:])
+# print(mylist[2:-1])
+# print(mylist[-1::-2])
+
